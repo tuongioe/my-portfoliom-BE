e501a08e7ad2de963779b499ee7f0e7bc3f26c32
Trương Quang Tường <21522777@gm.uit.edu.vn>
2025-10-07 21:35:29 +0700

    db.js update

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,14 @@ dotenv.config();
 
 const { Pool } = pg;
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL chưa được cấu hình");
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // cần cho Supabase
+    rejectUnauthorized: false,
   },
 });
 
@@ -15,4 +19,5 @@ pool
   .connect()
   .then(() => console.log("✅ Kết nối Supabase thành công"))
   .catch((err) => console.error("❌ Lỗi kết nối Supabase:", err));
+
 export default pool;
